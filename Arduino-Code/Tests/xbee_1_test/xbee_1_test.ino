d85e370cde95e67f8928408a69b189fb29804d2a
LesleyWheat <wheatd@mcmaster.ca>
2019-12-02 22:24:12 -0500

    Rssi values test

--- Arduino-Code/Tests/xbee_1_test/xbee_1_test.ino
+++ Arduino-Code/Tests/xbee_1_test/xbee_1_test.ino
@@ -5,12 +5,18 @@ SoftwareSerial mySerial(3, 2); // RX, TX
 int led = 13;
 int received = 0;
 int i;
+
+int rssi_lowpass = 0;
+int rssi_raw = 0;
+int rssi_error = 0;
+int analogPin = A0;
+
 void setup() {
   Serial.begin(9600); 
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Native USB only
   }
-  Serial.println("Goodnight moon!\n");
+  //Serial.println("Goodnight moon!\n");
   // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);
   mySerial.println("Hello, world?\n");
@@ -21,8 +27,10 @@ void setup() {
 void loop() {
   if (mySerial.available() > 0) {
     received = mySerial.read();
-    
-    Serial.write(received);
+
+    rssi_raw = analogRead(analogPin)*5.0*1000/1024;
+    rssi_lowpass = (rssi_lowpass*90.0 + rssi_raw*10.0)/100.0;
+    Serial.println("rssi_raw " + String(rssi_raw) + " rssi_lowpass " + String(rssi_lowpass));
   }
 
   if (Serial.available() > 0){
