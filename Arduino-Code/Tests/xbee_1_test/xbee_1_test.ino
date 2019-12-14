15b74a2311391fb75d3e427a36d010ef4b8043f0
LesleyWheat <wheatd@mcmaster.ca>
2019-12-14 09:17:51 -0500

    Xbee test now uses pulsein instead of analogread

--- Arduino-Code/Tests/xbee_1_test/xbee_1_test.ino
+++ Arduino-Code/Tests/xbee_1_test/xbee_1_test.ino
@@ -10,8 +10,13 @@ int rssi_lowpass = 0;
 int rssi_raw = 0;
 int rssi_error = 0;
 int analogPin = A0;
+byte PWM_PIN = 3;
+int pwm_value_high;
+int pwm_value_low;
 
 void setup() {
+  pinMode(PWM_PIN, INPUT);
+  
   Serial.begin(9600); 
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Native USB only
@@ -28,9 +33,17 @@ void loop() {
   if (mySerial.available() > 0) {
     received = mySerial.read();
 
+    pwm_value_high = pulseIn(PWM_PIN, HIGH);
+    pwm_value_low = pulseIn(PWM_PIN, LOW);
     rssi_raw = analogRead(analogPin)*5.0*1000/1024;
-    rssi_lowpass = (rssi_lowpass*90.0 + rssi_raw*10.0)/100.0;
-    Serial.println("rssi_raw " + String(rssi_raw) + " rssi_lowpass " + String(rssi_lowpass));
+
+    Serial.println("pwm_value_high " + String(pwm_value_high) + " pwm_value_low " + String(pwm_value_low)+ "rssi_raw " + String(rssi_raw));
+
+    //rssi_raw = analogRead(analogPin)*5.0*1000/1024;
+    //rssi_lowpass = (rssi_lowpass*90.0 + rssi_raw*10.0)/100.0;
+    //Serial.println("rssi_raw " + String(rssi_raw) + " rssi_lowpass " + String(rssi_lowpass));
+
+    
   }
 
   if (Serial.available() > 0){
